a77148615f8ee931ba47e82f0f0528b4b828f5ce
LucasMatheusW <westphallucas1@outlook.com>
2019-10-09 20:22:27 -0300

    move pato

---
+++ Ducktective/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private float speed;
+    private Vector2 direction;
+    public Animator animate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2;
+        direction = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputCharacter();
+        transform.Translate(direction * speed * Time.deltaTime);
+        AnimateChar(direction);
+    }
+
+    void InputCharacter()
+    {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+    void AnimateChar(Vector2 dir)
+    {
+        animate.SetFloat("x", dir.x);
+        animate.SetFloat("y", dir.y);
+    }
+}
