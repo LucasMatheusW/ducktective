1a5dc060183dff167c66275316d91c9be60a5756
LucasMatheusW <westphallucas1@outlook.com>
2019-11-16 17:24:03 -0300

    dialogo

--- Ducktective/Assets/Scripts/DialogScript.cs
+++ Ducktective/Assets/Scripts/DialogScript.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fungus;
 
 public class DialogScript : MonoBehaviour
 {
+    private bool isOnDialogArea;
+    [SerializeField]
+    private Flowchart fc;
+    [SerializeField]
+    private GameObject character;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOnDialogArea = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isOnDialogArea)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {   
+                if(character.tag == "Clyde"){
+                    fc.ExecuteBlock("ClydeDialog");
+                }          
+            }   
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ducktective"))
+        {
+            isOnDialogArea = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Ducktective"))
+        {
+            isOnDialogArea = false;
+        }
     }
 }
